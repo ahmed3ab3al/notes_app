002342b570ef62f0b0f43095ae9fdb78952095e6
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-11-12 21:26:18 +0200

    generate typeAdapter

--- lib/features/home/data/models/note_model.dart
+++ lib/features/home/data/models/note_model.dart
@@ -1,12 +1,23 @@
-class NoteModel {
+import 'package:hive_flutter/hive_flutter.dart';
+part 'note_model.g.dart';
+
+
+@HiveType(typeId: 0)
+class NoteMode extends HiveObject   {
+
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String content;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
-  NoteModel(
-      {required this.title,
-      required this.content,
-      required this.date,
-      required this.color});
+  NoteMode({
+    required this.title,
+    required this.content,
+    required this.date,
+    required this.color});
+
 }
