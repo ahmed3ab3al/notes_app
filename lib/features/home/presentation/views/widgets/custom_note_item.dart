ff59344709bac22755cd96770b34434149a43f8d
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-11-15 19:22:28 +0200

    display data

--- lib/features/home/presentation/views/widgets/custom_note_item.dart
+++ lib/features/home/presentation/views/widgets/custom_note_item.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/features/home/data/models/note_model.dart';
 import 'package:notes_app/features/home/presentation/views/edit_view.dart';
 
 class CustomNoteItem extends StatelessWidget {
-  const CustomNoteItem({super.key});
+  const CustomNoteItem({super.key, required this.model});
+  final NoteMode model;
 
   @override
   Widget build(BuildContext context) {
@@ -20,14 +22,14 @@ class CustomNoteItem extends StatelessWidget {
           left: 12,
         ),
         decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
+          color:  Color(model.color),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title: const Text('Flutter Tips',
+              title:  Text(model.title,
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: 26,
@@ -35,7 +37,7 @@ class CustomNoteItem extends StatelessWidget {
               subtitle: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16),
                 child:
-                    Text('Build your career with Flutter and learn new things.',
+                    Text(model.content,
                         style: TextStyle(
                           color: Colors.black.withOpacity(.5),
                           fontSize: 16,
@@ -57,7 +59,7 @@ class CustomNoteItem extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(right: 24),
-              child: Text('9 days ago',
+              child: Text(model.date,
                   style: TextStyle(
                     color: Colors.black.withOpacity(.4),
                     fontSize: 13,
--- lib/features/home/presentation/views/widgets/custom_notes_list_view.dart
+++ lib/features/home/presentation/views/widgets/custom_notes_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/features/home/data/models/note_model.dart';
 import 'package:notes_app/features/home/presentation/view_models/cubit/read_notes_cubit/read_notes_cubit.dart';
 import 'package:notes_app/features/home/presentation/view_models/cubit/read_notes_cubit/read_notes_state.dart';
 import 'package:notes_app/features/home/presentation/views/widgets/custom_note_item.dart';
@@ -11,14 +12,17 @@ class CustomNotesListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ReadNotesCubit, ReadNotesState>(
       builder: (context, state) {
+        List<NoteMode> notes =ReadNotesCubit.get(context).notes!;
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 14),
           child: ListView.builder(
-            itemCount: ReadNotesCubit.get(context).notes!.length,
+            itemCount: notes.length,
             itemBuilder: (BuildContext context, int index) {
-              return const Padding(
+              return  Padding(
                 padding: EdgeInsets.symmetric(vertical: 4),
-                child: CustomNoteItem(),
+                child: CustomNoteItem(
+                  model: notes[index],
+                ),
               );
             },),
         );
