76cabf126b494e933a9fb6297b1bedda30a88611
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2026-06-13 15:15:29 +0300

    init app

---
+++ lib/views/notesView.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/notes_view_body.dart';
+
+class Notesview extends StatelessWidget {
+  const Notesview({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: NotesViewBody());
+  }
+}
