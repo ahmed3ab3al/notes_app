9bd27b7b85d2701f401edbebd933075037e09276
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2026-06-14 21:07:17 +0300

    notes_list_view

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
 import 'package:notes_app/views/widgets/custom_note_item.dart';
+import 'package:notes_app/views/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -10,7 +11,11 @@ class NotesViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(24),
       child: const Column(
-        children: [SizedBox(height: 25), CustomAppbar(), CustomNoteItem()],
+        children: [
+          SizedBox(height: 25),
+          CustomAppbar(),
+          Expanded(child: NotesListView()),
+        ],
       ),
     );
   }
