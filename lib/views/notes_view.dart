0c40291b4f46255abfb178b8b92c34ea07525645
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2026-06-15 22:18:18 +0300

    add border radius to sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -3,22 +3,19 @@ import 'package:notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 class Notesview extends StatelessWidget {
-  Notesview({super.key});
-  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  const Notesview({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: scaffoldKey,
       body: NotesViewBody(),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue,
         shape: CircleBorder(),
         onPressed: () {
-          // scaffoldKey.currentState!.showBottomSheet((context) {
-          //   return Container(height: 50);
-          // }).closed.then((value){});
-
           showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
             context: context,
             builder: (context) {
               return AddNoteBottomSheet();
